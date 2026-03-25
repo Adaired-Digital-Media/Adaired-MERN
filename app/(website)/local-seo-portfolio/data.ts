b4c1379fefb0707af8b0fa1f26336b4e26e9f4ed
Akshay G <akshay.mittal@adaired.org>
2026-03-25 17:02:28 +0530

    local seo portfolio created

---
+++ app/(website)/local-seo-portfolio/data.ts
@@ -0,0 +1,220 @@
+export type Section = {
+    name: string;
+    websiteLink: string;
+    theme: "purple" | "purpleLight" | "green" | "orange" | "pink";
+    keywords: string[];
+    initial: string[];
+    current: string[];
+};
+
+export const data = {
+    hvac: {
+        name: "Hvac",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "purple",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    pestControlservices: {
+        name: "Pest Control Services",
+        websiteLink: "https://bugandtermitecontrol.com/",
+        theme: "purpleLight",
+        keywords: [
+            "Boat Pest Control in Florida",
+            "Tree Doctor Treatment",
+            "Pest and Bug Control Edgewater",
+            "Affordable Pest Control",
+            "Home Tree Doctor Treatment",
+        ],
+        initial: ["Not in 100", "Not in 100", "59th", "Not in 100", "Not in 100"],
+        current: ["1", "1", "2", "1", "1"],
+    },
+
+    digitalProducts: {
+        name: "Digital Products / Online Services",
+        websiteLink: "https://bulkaccounts.com/",
+        theme: "green",
+        keywords: [
+            "Buy Bulk Email Accounts",
+            "Buy Bulk Accounts",
+            "Buy Old Apple ID Accounts",
+            "Bulk Outlook Accounts",
+            "Buying Bulk Instagram Accounts",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "1", "3", "4", "2"],
+    },
+
+    interiorDesign: {
+        name: "Home Improvement & Interior Design",
+        websiteLink: "https://bluechipshuttersblinds.com/",
+        theme: "orange",
+        keywords: [
+            "Custom Shutters and Blinds",
+            "Window Treatments Ormond",
+            "Shutter Company Ormond",
+            "Roller Shades Ormond",
+            "Shutter Company Ormond Beach",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "1", "1", "1", "3"],
+    },
+
+    food: {
+        name: "Food",
+        websiteLink: "https://www.crispychicken.com/",
+        theme: "pink",
+        keywords: [
+            "Chicken Nuggets Miami",
+            "Best Chicken Fries Miami",
+            "Best Chicken Nuggets in Miami",
+            "Best Chicken Sandwich Miami",
+            "Chicken Tenders Delray",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["2", "3", "2", "3", "3"],
+    },
+
+    digitalMarketing: {
+        name: "Digital marketing",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "purple",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    chiropractic: {
+        name: "Chiropractic",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "purpleLight",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    onlineReputation: {
+        name: "Online Reputation",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "green",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    homeImprovement: {
+        name: "Home improvement",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "orange",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    medicalAndBiohazard: {
+        name: "Medical & Biohazard",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "pink",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    mortgageAdvisory: {
+        name: "Mortgage Advisory & Brokerage Services",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "purple",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    chiropractic2: {
+        name: "Chiropractic",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "purpleLight",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    homeImprovementServices: {
+        name: "Home improvement services",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "green",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+
+    webDevelopment: {
+        name: "Web development",
+        websiteLink: "https://cunninghamoil.com/",
+        theme: "orange",
+        keywords: [
+            "Gas Grills Daytona Beach",
+            "AC Daytona Beach",
+            "RV Propane Filling Stations",
+            "Air Conditioning Repair Financing",
+            "Propane Grill Repair Near Me",
+        ],
+        initial: ["Not in 100", "Not in 100", "Not in 100", "Not in 100", "Not in 100"],
+        current: ["1", "2", "5", "6", "3"],
+    },
+} satisfies Record<string, Section>;
