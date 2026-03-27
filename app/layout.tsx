51c30cb81d12961690ae1c4c94646dd2e815f77f
anujaraired <anuj@adaired.org>
2026-03-27 15:11:51 +0530

    add bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import { Poppins, Outfit, Montserrat, Mr_Dafoe } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script";
-import Link from "next/link";
-import Image from "next/image";
-import whatsApp from '../public/assets/icons/whatsapp.png';
 
 /* =========================
    FONT CONFIGURATION
@@ -56,24 +52,6 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
         <Toaster position="top-right" reverseOrder={false} />
-        <div>
-          <div className="fixed bottom-2 left-3 z-40 cursor-pointer">
-            <Link href="https://wa.me/918907300008" target="_blank">
-              <Image
-                src={whatsApp}
-                width={55}
-                height={55}
-                alt="WhatsApp"
-                className="h-[2.8rem] w-[2.8rem] md:h-[3.2rem] md:w-[3.2rem] lg:h-[3.438rem] lg:w-[3.438rem]"
-              />
-            </Link>
-          </div>
-          <Script
-            id="zsiqscript"
-            src="https://salesiq.zohopublic.com/widget?wc=siq7daf32053a2c9ad94317715612d7eb575af5e85bd3e25754067471d73684b3b9"
-            strategy="lazyOnload"
-          />
-        </div>
       </body>
     </html>
   );
